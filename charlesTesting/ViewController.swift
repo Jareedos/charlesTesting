259c01c77c69a9c3b318ecda8163ddb1a204c3a7
Jareedos <jaredallens@gmail.com>
2017-04-18 17:01:52 -0700

    Finished UI layout for LoginVC

--- charlesTesting/ViewController.swift
+++ charlesTesting/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTxtField: UITextField!
+    @IBOutlet weak var LoginBtn: UIButton!
+    @IBOutlet weak var forgotPasswordBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+ 
+    @IBAction func logInBtnPressed(_ sender: Any) {
+    }
 
+    @IBAction func forgotPasswordBtnPressed(_ sender: UIButton) {
+    }
 
 }
 
