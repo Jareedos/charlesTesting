f146013895bd6b8339c40eb304bc6dd26caca7ff
Jareedos <jaredallens@gmail.com>
2017-04-28 16:11:53 -0700

    Created Custom Slide in Right to Left

---
+++ charlesTesting/RightLeftSegue.swift
@@ -0,0 +1,33 @@
+//
+//  RightLeftSegue.swift
+//  charlesTesting
+//
+//  Created by Jared Sobol on 4/28/17.
+//  Copyright © 2017 Appmaker. All rights reserved.
+//
+
+import UIKit
+
+class RightLeftSegue: UIStoryboardSegue {
+    override func perform()
+    {
+        let src = self.source as UIViewController
+        let dst = self.destination as UIViewController
+        
+        src.view.superview?.insertSubview(dst.view, aboveSubview: src.view)
+        dst.view.transform = CGAffineTransform(translationX: src.view.frame.size.width, y: 0)
+        
+        UIView.animate(withDuration: 0.28,
+                                   delay: 0.0,
+                                   options: UIViewAnimationOptions.curveEaseInOut,
+                                   animations: {
+                                    dst.view.transform = CGAffineTransform(translationX: 0, y: 0)
+        },
+                                   completion: { finished in
+                                    src.present(dst, animated: false, completion: nil)
+        }
+        )
+    }
+    
+
+}
